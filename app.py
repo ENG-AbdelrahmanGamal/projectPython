cdfaa319f01aa22220797a691633dbbaafef8e6c
Abdelrahman Gamal <abdelrahmanbetter@stud.cu.edu.eg>
2022-06-16 07:36:23 +0200

    add conditional function

---
+++ app.py
@@ -0,0 +1,10 @@
+is_cold = True
+is_hot= True
+if is_hot:
+    print("the weather is hot ")
+    print("please take some drink's")
+elif is_cold:
+    print("the weather is cold")
+    print("wear the warm clothes")
+else:
+    print("the weather is lovely")
