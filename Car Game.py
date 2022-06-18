71a7464f20600d07d1d0787f8ae7e590a0735257
Abdelrahman Gamal <abdelrahmanbetter@stud.cu.edu.eg>
2022-06-18 12:01:45 +0200

    add boolean variable to check if it alrady to in the same status

--- Car Game.py
+++ Car Game.py
@@ -1,17 +1,32 @@
-action=input("> ")
-while(action == "help"):
-   # if action=="help"or action=="Help":
-    print("start :(to start car move)")
-    print("stop :(to stop car moving)")
-    print("Exit :(go to exit your game)")
-    your_choies = input(">")
-    if your_choies == "start":
-        print("your car is start to moving ")
-    elif your_choies == "stop":
-        print("your car is stopped..")
-    elif your_choies == "exit":
+
+started=False
+while True:
+    your_choice = input(">").lower()
+    if your_choice == "start":
+        if started:
+         print("your car already started")
+        else:
+            started=True
+            print("your car is start to moving ")
+    elif your_choice== "stop":
+        if not started:
+            print("your car already Stopped")
+        else:
+          started=False
+          print("your car is stopped..")
+    elif your_choice == "exit":
         print(" exit")
         break
+    elif your_choice== "help":
+        print("""
+ start :(to start car move)
+ stop  :(to stop car moving)
+ Exit  :(go to exit your game)
+            """)
+    else:
+        print("i don't under stand that :!! ")
+
+
 else:
     print("i don't under stand that :!! ")
 
